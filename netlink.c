AkihiroSuda/passt-mirror

commit 450a6131beabd6537f2460bcc110a9a961697649

netlink: With no default route, pick the first interface with a route
While commit f919dc7a4b1c ("conf, netlink: Don't require a default
route to start") sounded reasonable in the assumption that, if we
don't find default routes for a given address family, we can still
proceed by selecting an interface with any route *iff it's the only
one for that protocol family*, Jelle reported a further issue in a
similar setup.

There, multiple interfaces are present, and while remote container
connectivity doesn't matter for the container, local connectivity is
desired. There are no default routes, but those multiple interfaces
all have non-default routes, so we should just pick one and start.

Pick the first interface reported by the kernel with any route, if
there are no default routes. There should be no harm in doing so.

Reported-by: Jelle van der Waa <jvanderwaa@redhat.com>
Reported-by: Martin Pitt <mpitt@redhat.com>
Link: https://bugzilla.redhat.com/show_bug.cgi?id=2277954
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
Reviewed-by: Paul Holzinger <pholzing@redhat.com>

netlink.c
@@ -269,8 +269,7 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 	size_t na;
 
 	/* Look for an interface with a default route first, failing that, look
-	 * for any interface with a route, and pick it only if it's the only
-	 * interface with a route.
+	 * for any interface with a route, and pick the first one, if any.
 	 */
 	seq = nl_send(s, &req, RTM_GETROUTE, NLM_F_DUMP, sizeof(req));
 	nl_foreach_oftype(nh, status, s, buf, seq, RTM_NEWROUTE) {
@@ -324,18 +323,19 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 		warn("netlink: RTM_GETROUTE failed: %s", strerror(-status));
 
 	if (defifi) {
-		if (ndef > 1)
+		if (ndef > 1) {
 			info("Multiple default %s routes, picked first",
 			     af_name(af));
+		}
 		return defifi;
 	}
 
 	if (anyifi) {
-		if (nany == 1)
-			return anyifi;
-
-		info("Multiple interfaces with %s routes, use -i to select one",
-		     af_name(af));
+		if (nany > 1) {
+			info("Multiple interfaces with %s routes, picked first",
+			     af_name(af));
+		}
+		return anyifi;
 	}
 
 	if (!nany)

passt.1
@@ -149,8 +149,8 @@ This option can be specified zero (for defaults) to two times (once for IPv4,
 once for IPv6).
 By default, assigned IPv4 and IPv6 addresses are taken from the host interfaces
 with the first default route, if any, for the corresponding IP version. If no
-default routes are available and there is just one interface with any route,
-that interface will be chosen instead.
+default routes are available and there is any interface with any route for a
+given IP version, the first of these interfaces will be chosen instead.
 
 .TP
 .BR \-n ", " \-\-netmask " " \fImask

qemus/passt

commit 7e8d49b8a6b501b9eb9caf44cf1f653fd2a385b5

netlink: Don't warn about multiple interfaces when there's only one
Suppose there is a single network interface on the host, with two
addresses of different prefixes assigned to it.

In this situation, running pasta to create an isolated netns prints
"Multiple interfaces with %s routes, picked first", even though there
is in fact only one candidate interface.

Reproducer:

  sudo ip link add dummy42 type dummy
  sudo ip link set dev dummy42 up
  sudo ip addr add fdaa:aaaa:aaaa::1/64 dev dummy42
  sudo ip addr add fdbb:bbbb:bbbb::1/64 dev dummy42

  pasta --config-net -- true

This message is meant to indicate that there are multiple candidate
interfaces, but in fact there is only one, which can mislead users.

Therefore, avoid printing this message when there are multiple non-default
routes that all belong to the same interface.

Fixes: 450a6131beab ("netlink: With no default route, pick the first interface with a route")
Signed-off-by: Hayato Kiwata <dev@haytok.jp>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

netlink.c
@@ -271,6 +271,7 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 	};
 	unsigned defifi = 0, anyifi = 0;
 	unsigned ndef = 0, nany = 0;
+	bool multi_anyif = false;
 	struct nlmsghdr *nh;
 	struct rtattr *rta;
 	char buf[NLBUFSIZ];
@@ -330,6 +331,8 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 			nany++;
 			if (!anyifi)
 				anyifi = thisifi;
+			else if (anyifi != thisifi)
+				multi_anyif = true;
 		}
 	}
 
@@ -345,7 +348,7 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 	}
 
 	if (anyifi) {
-		if (nany > 1) {
+		if (multi_anyif) {
 			info("Multiple interfaces with %s routes, picked first",
 			     af_name(af));
 		}